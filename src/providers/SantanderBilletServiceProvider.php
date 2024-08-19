b2f744db13802863cd67bcb25b933ae8289ef382
devpaulopaixao <paulinhohenrique90@yahoo.com.br>
2024-08-19 11:27:28 -0300

    fix: Santander billet config file error

--- src/providers/SantanderBilletServiceProvider.php
+++ src/providers/SantanderBilletServiceProvider.php
@@ -9,7 +9,7 @@ class SantanderBilletServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '../config/santander_billet.php' => config_path('santander_billet.php'),
+            __DIR__ . '../../config/santander_billet.php' => config_path('santander_billet.php'),
         ], 'config');
     }
 
