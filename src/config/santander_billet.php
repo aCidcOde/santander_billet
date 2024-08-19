67962e9adbd27ceee2d387c773451c4918cec51d
devpaulopaixao <paulinhohenrique90@yahoo.com.br>
2024-08-19 12:27:35 -0300

    fix: Santander billet conif file integrations wrong node

--- src/config/santander_billet.php
+++ src/config/santander_billet.php
@@ -2,13 +2,11 @@
 
 return [
     'integrations' => [
-        [
-            'environment' => env('SANTANDER_BILLET_ENVIRONMENT', 'sandbox'),
-            'host' => env('SANTANDER_BILLET_ENVIRONMENT') === 'sandbox' ? 'https://trust-sandbox.api.santander.com.br' : 'https://trust-open.api.santander.com.br',
-            'client_id' => env('SANTANDER_BILLET_CLIENT_ID'),
-            'client_secret' => env('SANTANDER_BILLET_CLIENT_SECRET'),
-            'certificate_auth' => env('SANTANDER_BILLET_CERTIFICATE_AUTH'),
-            'certificate_path' => env('SANTANDER_BILLET_CERTIFICATE_PATH'),
-        ]
+        'environment' => env('SANTANDER_BILLET_ENVIRONMENT', 'sandbox'),
+        'host' => env('SANTANDER_BILLET_ENVIRONMENT') === 'sandbox' ? 'https://trust-sandbox.api.santander.com.br' : 'https://trust-open.api.santander.com.br',
+        'client_id' => env('SANTANDER_BILLET_CLIENT_ID'),
+        'client_secret' => env('SANTANDER_BILLET_CLIENT_SECRET'),
+        'certificate_auth' => env('SANTANDER_BILLET_CERTIFICATE_AUTH'),
+        'certificate_path' => env('SANTANDER_BILLET_CERTIFICATE_PATH')
     ]
 ];
